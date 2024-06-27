0abdaa6b19f78b5dc76aaf2f3c47d56b02e306a3
Neeraj T.R <149990701+neerajsonu006@users.noreply.github.com>
2024-06-27 07:33:55 +0530

    Update Student.py

--- Student.py
+++ Student.py
@@ -1,30 +1,65 @@
 class Student:
-    def __init__(self, name, usn, m1,m2,m3):
+    def _init_(self, name, roll_num, marks):
         self.name = name
-        self.usn = usn
-        self.m1 = m1
-        self.m2 = m2
+        self.roll_num = roll_num
+        self.marks = marks
+        self.avg_mark = sum(marks) / len(marks)
+    
+    def _str_(self):
+        return f"Name: {self.name}, Roll Number: {self.roll_num}, Average Mark: {self.avg_mark}"
 
-        self.m3 = m3
+def student_details():
+    students = []
+    for i in range(1, 2):
+        print(f"Enter details for student {i}:")
+        name = input("Name: ")
+        roll_num = input("Roll Number: ")
+        marks = []
+        for j in range(3):  
+            mark = float(input(f"Enter marks for Subject {j+1}: "))
+            marks.append(mark)
+        student = Student(name, roll_num, marks)
+        students.append(student)
+        print()  
+    return students
+
+def min_avg(students):
+    min_avg = students[0]  
+    min_avg = min_avg.avg_mark
+
+    for student in students[1:]:
+        if student.avg_mark < min_avg:
+            min_avg = student.avg_mark
+            min_avg = student
+
+    print(f"Student with min avg marks:")
+    print(min_avg)
+
+def max_avg(students):
+    max_avg = students[0]
+    max_avg = max_avg.avg_mark
+
+    for student in students[1:]:
+        if student.avg_mark > max_avg:
+            max_avg = student.avg_mark
+            max_avg = student
+
+    print(f"Student with max avg marks:")
+    print(max_avg)
+
+def rankwise_students(students):
+    ranked_students = sorted(students, key=lambda student:student.avg_mark, reverse=True)
+    print("Rankwise list of students based on average marks:")
+    rank = 1
+    for student in ranked_students:
+        print(f"Rank {rank}: {student}")
+        rank += 1
+        if _name_ == "_main_":
+            students = student_details()
 
 
-    def __str__(self):
-        return f"Name: {self.name}, USN: {self.usn}, Mark1: {self.m1},Mark2: {self.m2},Mark3: {self.m3}"
-
-st_dict = {}
-
-num = int(input("Enter the number of student"))
-
-for i in range(num):
-    name = input(f"Enter name for student {i+1}: ")
-    age = input(f"Enter usn for student {i+1}: ")
-    m1 = input(f"Enter marks of student subject 1: ")
-    m2 = input(f"Enter marks of student subject 2: ")
-    m3 = input(f"Enter marks of student subject 3: ")
-
-    student = Student(name, age, m1,m2,m3)
-    st_dict[f"Student {i+1}"] = student
-
-print("\nStudents are :")
-for key, student in st_dict.items():
-    print(f"{key}: {student}")
+  min_avg(students)
+  max_avg(students)
+  rankwise_students(students)
+    
+    
